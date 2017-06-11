13b56a5836c3c8b9891b9e47bfc93dbc87a0703a
Mattoni <amattoni@petrichor.io>
2017-06-10 23:17:13 -0700

    update account tests

--- test/account/index.ts
+++ test/account/index.ts
@@ -1,12 +1,45 @@
-// import { assert } from "chai";
+import { assert } from "chai";
 import { Account } from "../../src/";
 import { AccessToken } from "../common";
+
+const AccountSchema = {
+    title: "Account",
+    type: "object",
+    definitions: {
+        name: {
+            type: "object",
+            properties: {
+                first: { type: "string" },
+                last: { type: "string" },
+            },
+        },
+        email: {
+            type: "object",
+            properties: {
+                address: { type: "string" },
+                verified: { type: "boolean" },
+            },
+        },
+    },
+    properties: {
+        id: { type: "string" },
+        name: { $ref: "#/definitions/name" },
+        email: {$ref: "#/definitions/email"},
+        temp: { type: "boolean" },
+    },
+};
 
 export function testAccount() {
     describe("Account", () => {
         it("should fetch user account", async () => {
-            const resp = await Account.getSingle(AccessToken, {}, {url: process.env.API_URL});
-            console.log(resp);
+            const resp = await Account.getSingle(AccessToken, {}, { url: process.env.API_URL });
+            if (!resp.ok) {
+                throw new Error(resp.error.title);
+            }
+
+            assert.isTrue(resp.ok);
+            assert.isObject(resp.value.data);
+            assert.jsonSchema(resp.value.data, AccountSchema);
         });
     });
 }
--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,10 @@
+import * as chai from "chai";
+import * as jsonSchema from "chai-json-schema";
 import { config } from "dotenv";
 import { testAccount } from "./account";
 import { passwordGrant } from "./auth/PasswordGrant";
+
+chai.use(jsonSchema);
 
 config({path: "./config.env"});
 
