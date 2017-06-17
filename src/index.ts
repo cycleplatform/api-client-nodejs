a62fa982a2ec0fc5f5a3de4074c36331d7ed841a
Mattoni <amattoni@petrichor.io>
2017-06-17 14:08:12 -0700

    export errors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import "isomorphic-fetch";
 import * as Auth from "./auth";
 import * as API from "./common/Api";
+import * as Error from "./common/Error";
 import { QueryParams } from "./common/QueryParams";
 import * as Structs from "./common/structs";
 import * as Account from "./resources/accounts";
 
-export { API, Auth, Account, QueryParams, Structs };
+export { API, Auth, Account, QueryParams, Structs, Error };
