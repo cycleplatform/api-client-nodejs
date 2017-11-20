172b94e247d689545d9651d50fc297397f6fb5c3
Mattoni <amattoni@petrichor.io>
2017-11-20 13:07:54 -0800

    add stage to pipeline event struct

--- src/pipeline/event.ts
+++ src/pipeline/event.ts
@@ -3,6 +3,7 @@ import { ResourceId } from "../common/structs";
 export interface PipelineEvent<T extends string> {
     header: T;
     id: ResourceId;
+    stage: "started" | "completed";
     project_id: ResourceId;
     environment_id: ResourceId;
     account_id: ResourceId;
