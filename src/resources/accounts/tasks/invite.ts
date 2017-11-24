177bf1252d11d047b8dbe588e7f42bc921f7d8eb
Alexander Mattoni <amattoni@petrichor.io>
2017-11-24 15:47:00 -0800

    add invite/decline convenience methods

--- src/resources/accounts/tasks/invite.ts
+++ src/resources/accounts/tasks/invite.ts
@@ -5,6 +5,50 @@ import { ResourceId, Task, CreatedTask } from "../../../common/structs";
 
 export type InviteAction = "accept" | "decline";
 
+export async function accept({
+    id,
+    token,
+    query,
+    settings,
+}: {
+    id: ResourceId;
+    token: Token;
+    query?: QueryParams;
+    settings?: Settings;
+}) {
+    return task({
+        id,
+        token,
+        query,
+        settings,
+        value: {
+            action: "accept",
+        },
+    });
+}
+
+export async function decline({
+    id,
+    token,
+    query,
+    settings,
+}: {
+    id: ResourceId;
+    token: Token;
+    query?: QueryParams;
+    settings?: Settings;
+}) {
+    return task({
+        id,
+        token,
+        query,
+        settings,
+        value: {
+            action: "decline",
+        },
+    });
+}
+
 export async function task({
     id,
     token,
@@ -13,7 +57,6 @@ export async function task({
     settings,
 }: {
     id: ResourceId;
-    project: ResourceId;
     token: Token;
     value: Task<InviteAction>;
     query?: QueryParams;
