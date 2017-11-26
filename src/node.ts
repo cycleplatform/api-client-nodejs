135e152f6f99d9627feea5e5c748f48534e00320
Alexander Mattoni <amattoni@petrichor.io>
2017-11-25 16:08:46 -0800

    fix server version exports

--- src/node.ts
+++ src/node.ts
@@ -1,5 +1,6 @@
-import "./index";
 if (typeof window === "undefined") {
     // tslint:disable-next-line:no-var-requires
     (global as any).WebSocket = require("ws");
 }
+
+export * from "./index";
