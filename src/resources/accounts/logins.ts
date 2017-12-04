c8e073702735b81541220be4c6bf3230d05b77ef
Alexander Mattoni <amattoni@petrichor.io>
2017-12-03 18:35:54 -0800

    account logins

--- src/resources/accounts/logins.ts
+++ src/resources/accounts/logins.ts
@@ -7,9 +7,11 @@ import {
     Time,
     ResourceId,
 } from "../../common/structs";
+import { PublicAccount } from "./account";
 
-export type Collection = CollectionDoc<Login>;
+export type Collection = CollectionDoc<Login, {}, LoginIncludes>;
 export type LoginType = "password" | "employee";
+export type LoginQuery = QueryParams<keyof LoginIncludes>;
 
 export interface Login extends Resource {
     account: AccountInfo;
@@ -35,13 +37,19 @@ export interface PublicEmployeeInfo {
     id: ResourceId;
 }
 
+export interface LoginIncludes {
+    accounts: {
+        [key: string]: PublicAccount;
+    };
+}
+
 export async function getCollection({
     token,
     query,
     settings,
 }: {
     token: Token;
-    query?: QueryParams;
+    query?: LoginQuery;
     settings?: Settings;
 }) {
     return Request.getRequest<Collection>({
