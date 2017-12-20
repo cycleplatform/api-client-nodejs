4c8afcb939eaeede4423cf3c0b63f863b19ffb63
Mattoni <amattoni@petrichor.io>
2017-12-20 11:31:44 -0600

    add namespaces

--- src/resources/stacks/spec/runtime.ts
+++ src/resources/stacks/spec/runtime.ts
@@ -1,6 +1,10 @@
 export interface Runtime {
     command?: RuntimeCommand;
+    namespaces: Namespace;
+    privileged: boolean;
 }
+
+export type Namespace = "ipc" | "pid" | "uts" | "network" | "mount" | "user";
 
 export interface RuntimeCommand {
     path: string;
