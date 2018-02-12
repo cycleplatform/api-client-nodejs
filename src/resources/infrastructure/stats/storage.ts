6a2d010e9f1e3fbb45b2a4e9fb397e824004898b
Mattoni <amattoni@petrichor.io>
2018-02-12 14:06:39 -0800

    update infra storage

--- src/resources/infrastructure/stats/storage.ts
+++ src/resources/infrastructure/stats/storage.ts
@@ -1,5 +1,22 @@
+import { Megabytes } from "../../../common/structs";
+
 export interface StorageStats {
+    volume_groups: VolumeGroup[];
     mounts: { [key: string]: MountedFS };
+}
+
+export interface VolumeGroup {
+    name: string;
+    size: Megabytes;
+    volumes: Volume[];
+}
+
+export interface Volume {
+    name: string;
+    size: Megabytes;
+    data_percent: number;
+    meta_percent: number;
+    pool: string;
 }
 
 export interface MountedFS {
