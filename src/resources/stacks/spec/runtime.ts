a603012aba656b14ae529f0f5a40af895c05f138
Mattoni <amattoni@petrichor.io>
2018-02-26 13:54:06 -0800

    runtime capabilities

--- src/resources/stacks/spec/runtime.ts
+++ src/resources/stacks/spec/runtime.ts
@@ -3,6 +3,40 @@ export interface Runtime {
     namespaces: Namespaces[];
     environment_vars: { [key: string]: string };
     privileged: boolean;
+    capabilities: BaseCapabilities | PrivilegedCapabilities | null;
+}
+
+export enum BaseCapabilities {
+    CAP_CHOWN = "CAP_CHOWN",
+    CAP_FSETID = "CAP_FSETID",
+    CAP_DAC_OVERRIDE = "CAP_DAC_OVERRIDE",
+    CAP_FOWNER = "CAP_FOWNER",
+    CAP_SETFCAP = "CAP_SETFCAP",
+    CAP_SETGID = "CAP_SETGID",
+    CAP_SETUID = "CAP_SETUID",
+    CAP_KILL = "CAP_KILL",
+    CAP_MKNOD = "CAP_MKNOD",
+    CAP_NET_BIND_SERVICE = "CAP_NET_BIND_SERVICE",
+    CAP_AUDIT_WRITE = "CAP_AUDIT_WRITE",
+}
+
+export enum PrivilegedCapabilities {
+    CAP_SETPCAP = "CAP_SETPCAP",
+    CAP_DAC_READ_SEARCH = "CAP_DAC_READ_SEARCH",
+    CAP_NET_ADMIN = "CAP_NET_ADMIN",
+    CAP_NET_RAW = "CAP_NET_RAW",
+    CAP_SYS_ADMIN = "CAP_SYS_ADMIN",
+    CAP_SYS_MODULE = "CAP_SYS_MODULE",
+    CAP_SYS_NICE = "CAP_SYS_NICE",
+    CAP_SYS_PACCT = "CAP_SYS_PACCT",
+    CAP_SYS_PTRACE = "CAP_SYS_PTRACE",
+    CAP_SYS_RAWIO = "CAP_SYS_RAWIO",
+    CAP_SYS_RESOURCE = "CAP_SYS_RESOURCE",
+    CAP_SYSLOG = "CAP_SYSLOG",
+    CAP_AUDIT_CONTROL = "CAP_AUDIT_CONTROL",
+    CAP_AUDIT_READ = "CAP_AUDIT_READ",
+    CAP_IPC_LOCK = "CAP_IPC_LOCK",
+    CAP_IPC_OWNER = "CAP_IPC_OWNER",
 }
 
 export enum Namespaces {
