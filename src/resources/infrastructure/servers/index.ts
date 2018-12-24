51d6d157db04892f9d679a53dd9d51405eedc772
Alexander Mattoni <amattoni@petrichor.io>
2018-12-23 22:12:18 -0500

    reservation struct

--- src/resources/infrastructure/servers/index.ts
+++ src/resources/infrastructure/servers/index.ts
@@ -1,3 +1,4 @@
 export * from "./server";
 export * from "./console";
 export * from "./activating";
+export * from "./reservation";
---
+++ src/resources/infrastructure/servers/reservation.ts
@@ -0,0 +1,21 @@
+import { Resource, ResourceId, IP, State } from "../../../common/structs";
+import { Kind, IPNet } from "../../../resources/network";
+import { ProviderIdentifier } from "../provider";
+
+export interface Reservation extends Resource {
+  container_id: ResourceId;
+  project_id: ResourceId;
+  kind: Kind;
+  provider: Provider;
+  ip: IPNet;
+  gateway: IP;
+  netmask: IP;
+  network: IP;
+  state: State;
+}
+
+export interface Provider {
+  provider: ProviderIdentifier;
+  location: string;
+  reservation: string;
+}
