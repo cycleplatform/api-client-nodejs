8666f0fc221222633e2982d1122ee4a6ccf4cc74
Kevin <kevin@Kevins-MacBook-Pro.local>
2019-10-31 17:14:08 -0700

    Created announcment logic

---
+++ src/resources/announcements/announcement.ts
@@ -0,0 +1,43 @@
+import * as Request from "../../common/api/request";
+import {
+  Resource,
+  ResourceId,
+  OwnerScope,
+  State,
+  StandardEvents,
+  CollectionDoc,
+  SingleDoc,
+} from "../../common/structs";
+import { StandardParams, links } from "common/api";
+
+export type Collection = CollectionDoc<Announcement>;
+export type Single = SingleDoc<Announcement>;
+export type AnnouncementEvents = "created" | "deleted" | "updated";
+export type AnnouncementState = "live" | "deleted";
+
+export interface Announcement extends Resource {
+  id: ResourceId;
+  detail: string;
+  link: string | null;
+  events: StandardEvents;
+  creator: OwnerScope;
+  state: State<AnnouncementState>;
+}
+
+export async function getCollection(params: StandardParams) {
+  return Request.getRequest<Announcement>({
+    ...params,
+    target: links.announcements().collection(),
+  });
+}
+
+export async function getSingle(
+  params: StandardParams & {
+    id: ResourceId;
+  },
+) {
+  return Request.getRequest<Single>({
+    ...params,
+    target: links.announcements().single(params.id),
+  });
+}
