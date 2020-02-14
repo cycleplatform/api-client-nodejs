4e63c040b415a3fce28e38e5dc09fa6c34888243
Alexander Mattoni <amattoni@petrichor.io>
2020-02-14 13:30:22 -0800

    fix import error

--- src/resources/infrastructure/index.ts
+++ src/resources/infrastructure/index.ts
@@ -1,7 +1,6 @@
 import * as Providers from "./provider";
 import * as Servers from "./servers";
 import * as IPs from "./ips";
-import * as Storage from "./storage";
 import * as Summary from "./summary";
 
-export { Providers, Servers, IPs, Storage, Summary };
+export { Providers, Servers, IPs, Summary };
