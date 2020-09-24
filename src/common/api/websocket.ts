176d5bce049f6afd3d1b3d1c635aee15b7160259
Grady Saccullo <gsaccullo@petrichor.io>
2020-09-24 12:09:46 -0700

    mas changes

--- src/common/api/websocket.ts
+++ src/common/api/websocket.ts
@@ -1,6 +1,6 @@
 import { ApiResult, makeUrl } from "./request";
 import { Settings } from "./settings";
-import WS, { MessageEvent } from "isomorphic-ws";
+import WS from "isomorphic-ws";
 
 export interface SocketConnectParams<T> {
   target: string;
