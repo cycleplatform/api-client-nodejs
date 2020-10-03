2a4a7f0cb10f946cd44591d9a97ba9dfda0d8de6
Grady Saccullo <gsaccullo@petrichor.io>
2020-10-03 09:32:12 -0700

    updates to struct

--- src/resources/surveys/response.ts
+++ src/resources/surveys/response.ts
@@ -1,14 +1,22 @@
 import * as Request from "../../common/api/request";
 // import { Resource, SingleDoc } from "../../common/structs";
 import { links, StandardParams } from "../../common/api";
+import { Resource, SingleDoc } from "common/structs";
+export type Single = SingleDoc<Response>;
 
-export interface Response {
+export interface Response extends Resource {
   survey_id: string;
   account_id: string;
   responses: Responses;
 }
 
 export type Responses = Record<string, Record<string, Record<string, string>>>;
+
+export type postResponseParams = {
+  survey_id: string;
+  account_id: string;
+  responses: Responses;
+};
 
 export async function postResponse(
   params: StandardParams & {
