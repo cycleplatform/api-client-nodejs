b801ca52b508e3a9f1baeab815ecc57966786f70
Grady Saccullo <gsaccullo@petrichor.io>
2021-02-03 14:04:15 -0800

    forgot about shared types whoops

--- src/resources/pipelines/steps/index.ts
+++ src/resources/pipelines/steps/index.ts
@@ -4,5 +4,6 @@ import * as Image from "./image";
 import * as Stack from "./stack";
 import * as Environment from "./environment";
 
+export * from "./shared-types";
 export * from "./step";
 export { Other, Container, Image, Stack, Environment };
