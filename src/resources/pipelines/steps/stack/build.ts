1ca296ec0c50d19c8fcf44d0d5acc936f0224b09
Grady Saccullo <gsaccullo@petrichor.io>
2021-02-05 15:57:10 -0800

    updates

--- src/resources/pipelines/steps/stack/build.ts
+++ src/resources/pipelines/steps/stack/build.ts
@@ -1,8 +1,10 @@
 import { ResourceLocation } from "../shared-types";
 import { Instructions, About } from "../../../../resources/stacks/builds";
 
+export type CreateType = "direct";
 export interface Create {
   stack: ResourceLocation;
+  type: CreateType;
   instructions: Instructions;
   about?: About;
 }
