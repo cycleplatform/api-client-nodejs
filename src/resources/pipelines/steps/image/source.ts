65e481f9b6f242ceff0fabcb4f1a50aa78df8f5a
Grady Saccullo <gsaccullo@petrichor.io>
2021-02-05 15:33:14 -0800

    fix structs

--- src/resources/pipelines/steps/image/source.ts
+++ src/resources/pipelines/steps/image/source.ts
@@ -1,7 +1,9 @@
 import { Origin, About } from "../../../images";
 
+export type CreateType = "direct";
 export interface Create {
   name: string;
   about?: About;
   origin: Origin;
+  type: CreateType;
 }
