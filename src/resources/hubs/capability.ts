a8d7fde182c077036025041e0398e65dea3676db
Grady Saccullo <gsaccullo@petrichor.io>
2021-06-23 23:20:16 -0700

    update caps

--- src/resources/hubs/capability.ts
+++ src/resources/hubs/capability.ts
@@ -54,9 +54,7 @@ export type Capability =
   | "containers-instances-migrate"
 
   // stacks
-  | "stacks-create"
-  | "stacks-update"
-  | "stacks-delete"
+  | "stacks-manage"
   | "stacks-view"
   | "stacks-builds-manage"
 
