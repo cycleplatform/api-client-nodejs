33feef24c35d96a0526a30f1d36dd837bb2aa501
Nick <nmclean@cycle.io>
2021-08-09 12:14:59 -0500

    fixed webhook struct

--- src/resources/stacks/spec/v1/integrations.ts
+++ src/resources/stacks/spec/v1/integrations.ts
@@ -1,7 +1,7 @@
 import { Webhook } from "../../../../common/structs";
 
 export interface Integrations {
-  events?: Events;
+  webhooks?: Webhooks;
   lets_encrypt?: LetsEncrypt;
   files?: File[];
 }
@@ -13,6 +13,10 @@ export interface LetsEncrypt {
   key_path?: string;
   bundle_path?: string;
   additional_certs_path?: string;
+}
+
+export interface Webhooks {
+  events: Events;
 }
 
 export interface Events {
