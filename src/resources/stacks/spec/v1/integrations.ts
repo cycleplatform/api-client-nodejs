f74f9abb2cafa00ed68d5e2485619533508a3733
Grady <gsaccullo@petrichor.io>
2021-09-17 12:25:14 -0700

    feat(integrations): add backblaze b2 integration

--- src/resources/stacks/spec/v1/integrations.ts
+++ src/resources/stacks/spec/v1/integrations.ts
@@ -4,6 +4,7 @@ export interface Integrations {
   webhooks?: Webhooks;
   lets_encrypt?: LetsEncrypt;
   files?: File[];
+  backups?: Backups;
 }
 
 export interface LetsEncrypt {
@@ -29,4 +30,21 @@ export interface Events {
 export interface File {
   source: string;
   destination: string;
+}
+
+export interface Backups {
+  source: string;
+  backup: Backup;
+  restore: Restore | null;
+}
+
+export interface Backup {
+  command: string;
+  dir: string | null;
+  cron_string: string | null;
+}
+
+export interface Restore {
+  command: string;
+  dir: string | null;
 }
